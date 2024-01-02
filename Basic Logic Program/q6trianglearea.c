d20ba353a45ff4f5a3e6403aa393355d7aa72993
rishabhacejain <rishabhacejain@gmail.com>
2024-01-02 17:25:22 +0530

    New File Added

---
+++ Basic Logic Program/q6trianglearea.c
@@ -0,0 +1,14 @@
+// 6. Find area of Triangle Formula : A = 1/2 × b × h
+#include<stdio.h>
+int main()
+{
+    int b,h;
+    int area;
+
+    printf("Enter The Base and height of the Traingle \n");
+    scanf("%d""%d",&b, &h);
+    area=(b*h)/2;
+    printf("The Area of Triangle %d",area);
+
+    return 0;
+}
