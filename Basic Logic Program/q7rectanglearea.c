aa673103c02c39a9aad0c888ee6d9c5a8c24f394
rishabhacejain <rishabhacejain@gmail.com>
2024-01-02 17:42:56 +0530

    New File Added

---
+++ Basic Logic Program/q7rectanglearea.c
@@ -0,0 +1,15 @@
+// 7. Find area of Rectangle Formula : A=wl.
+#include<stdio.h>
+int main()
+{
+    int w,l;
+    float area;
+    printf("Enter The Width and length Of The rectangle\n");
+    scanf("%d""%d",&w,&l);
+    area=w*l;
+    printf("The area of the rectangle is %f",area);
+    
+    return 0;
+
+
+}
