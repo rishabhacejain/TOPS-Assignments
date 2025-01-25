76e3c3647773dfb0216c15c7ae93c751350713b9
rishabhacejain <153978390+rishabhacejain@users.noreply.github.com>
2025-01-25 20:38:57 +0530

    Add files via upload
    INCOMPLETE

---
+++ JS Assignment/02_JS_Variable_DataType.js
@@ -0,0 +1,20 @@
+
+        let stringVar = "Hello Javascript"; //string
+        let numberVar = 15;               //number
+        let booleanVar = true;           //boolean
+        let nullVar = null;
+        let undefinedVar;
+
+         console.log(stringVar);
+
+        console.log("Value:",stringVar,"| Data Type of",stringVar,"is",typeof stringVar);
+        console.log("Value:",numberVar,"| Data Type of",numberVar,"is",typeof numberVar);
+        console.log("Value:",booleanVar,"| Data Type of",booleanVar,"is",typeof booleanVar);
+        console.log("Value:",nullVar,"| Data Type of",nullVar,"is",typeof nullVar);
+        console.log("Value:",undefinedVar,"| Data Type of",undefinedVar,"is",typeof undefinedVar);
+
+
+
+
+
+
---
+++ JS Assignment/03_JS_Operators.js
@@ -0,0 +1,40 @@
+let a =15;
+let b =20;   //declaring two numbers
+
+// All The Arithmeatic Operation
+
+let addition = a+b;
+let subtraction = a-b;
+let multiplication = a*b;
+let division = a/b;
+
+console.log("Addition:"+ addition);
+console.log("Subtraction:"+ subtraction);
+console.log("Multiplication"+ multiplication);
+console.log("Division:"+ division);
+
+let isEqual = a===b;
+let isGreater = a > b;
+let isSmaller = a< b;
+
+console.log("Are the two numbers equal? " + isEqual);  // false
+console.log("Is a greater than b ? " + isGreater);    //false  
+console.log("Is a Smaller than b ? " + isSmaller); //true
+
+
+//logical operator
+
+let condition1 =  a > 10;
+let condition2 =  b < 5;
+let logicalAND = condition1 && condition2;
+let logicalOR = condition1 || condition2;
+
+console.log("Is a > 10 and b < 5 "+ logicalAND);
+console.log("Is a > 10 or b < 5 "+ logicalOR);
+
+
+
+
+
+
+
